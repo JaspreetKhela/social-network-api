0137583d5903a90b9f8a467078b8135d6e18288f
Jaspreet Khela <jaspreetkhela@Jaspreets-MacBook-Pro.local>
2021-09-16 21:41:54 -0400

    Modified the User model's fields

--- models/User.js
+++ models/User.js
@@ -10,14 +10,14 @@ const dateFormat = require('../utils/dateFormat');
 const UserSchema = new Schema({
     username: {
         type: String,
+        unique: true,
         required: true,
         trim: true 
     },
     email: {
         type: String,
         required: [true, 'Please enter Email Address'],
-        unique: true,
-        lowercase: true
+        unique: true
         // validate: {
         //     validator: () => Promise.resolve(false),
         //     message: 'Email validation failed'
